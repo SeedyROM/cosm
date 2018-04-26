fb45876a76d9aadd77f1718f0b649564dda423d2
Zack Kollar <rallokkcaz@gmail.com>
2018-04-26 09:35:32 -0700

    Fixed local development issue

--- api/cosm/settings.py
+++ api/cosm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'zsds6huzvmf2ln2$(#a&mq5_md)&de$c8s$21z8$=c=^f_xz28'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'] if DEBUG is True else None
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 
@@ -79,22 +79,23 @@ WSGI_APPLICATION = 'cosm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {  
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+if os.environ.get('DOCKER'):
+    DATABASES = {  
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    } 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-} 
 
 
 # Password validation
